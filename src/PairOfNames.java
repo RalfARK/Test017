ec8a8484592bce410179d846e774e3738e42708c
RalfARK <rafal_osiadacz@o2.pl>
2019-06-04 13:14:06 +0200

    end

--- src/PairOfNames.java
+++ src/PairOfNames.java
@@ -1,7 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
 public class PairOfNames {
     public static void main(String[] args) {
-        System.out.println("Adsasda");
 
+        Scanner in = new Scanner(System.in);
+        String n1, n2, szuk;
+        Map<String, String> mapaImion = new HashMap<>();
+
+        while (true) {
+            System.out.println("Podaj imie 1: ");
+            n1 = in.nextLine();
+            System.out.println("Podaj imie 2: ");
+            n2 = in.nextLine();
+            if (n1.equals("-") || n2.equals("-")) {
+                break;
+            }
+            mapaImion.put(n1, n2);
+        }
+        System.out.println("Wprowadzone pary imon: ");
+        Set<Map.Entry<String, String>> st = mapaImion.entrySet();
+        for (Map.Entry<String, String> mmm : st) {
+            System.out.print(mmm.getKey() + " : ");
+            System.out.println(mmm.getValue());
+        }
+
+        System.out.println("Podaj szukane imie: ");
+        szuk = in.nextLine();
+
+        if (mapaImion.containsKey(szuk)) {
+            System.out.println("para do szukanego imienia: " + mapaImion.get(szuk));
+        }
+        else {
+            System.out.println("nie ma szukanego imienia");
+        }
 
     }
 }
